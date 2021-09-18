2dd7582212795dd1f3c159c1f8f07d3bc120a2ec
Michail Spiridonov <michailspiridonov@lits-admins-MacBook-Pro.local>
2021-09-18 18:00:00 +0300

    fix test db in tests

--- tests.py
+++ tests.py
@@ -1,5 +1,7 @@
 import os
+import pytest
 
+import motor.motor_asyncio
 from pymongo import MongoClient
 
 from fastapi.testclient import TestClient
@@ -7,46 +9,55 @@ from fastapi.testclient import TestClient
 from main import app
 
 client = TestClient(app)
+
+async_db_client = motor.motor_asyncio.AsyncIOMotorClient(os.environ["MONGODB_URL"])
+async_db = async_db_client.test_local
+
 db_client = MongoClient(os.environ["MONGODB_URL"])
-db = db_client.local
+db = db_client.test_local
 
-def test_get_ad_increment():
-    ad = dict(
-        sdk_version="1.0",
-        session_id="001",
-        platform='test',
-        username='test_username',
-        country_code='001'
-    )
+TEST_AD = dict(
+    sdk_version="1.0",
+    session_id="001",
+    platform='test',
+    username='test_username',
+    country_code='001'
+)
 
-    response = client.post("/GetAd/", json=ad)
+
+@pytest.fixture(autouse=True)
+def run_before_and_after_tests(monkeypatch):
+    monkeypatch.setattr('db_utils.db', async_db)
+
+    yield
+
+    for collection_name in db.list_collection_names():
+        db[collection_name].drop()
+
+def test_get_ad_increment(monkeypatch):
+    response = client.post("/GetAd/", json=TEST_AD)
     assert response.status_code == 200
-    el =  db["stats"].find_one({"name": f"username-{ad['username']}"})
-    response = client.post("/GetAd/", json=ad)
+    el = db["stats"].find_one({"name": f"username-{TEST_AD['username']}"})
+    response = client.post("/GetAd/", json=TEST_AD)
     assert response.status_code == 200
-    update_el = db["stats"].find_one({"name": f"username-{ad['username']}"})
+    update_el = db["stats"].find_one({"name": f"username-{TEST_AD['username']}"})
     assert el['ad_requests'] + 1 == update_el['ad_requests']
 
 
 def test_impressions_increment():
-    ad = dict(
-        sdk_version="1.0",
-        session_id="001",
-        platform='test',
-        username='test_username',
-        country_code='001'
-    )
-
-    response = client.post("/Impression/", json=ad)
+    response = client.post("/Impression/", json=TEST_AD)
     assert response.status_code == 200
-    el = db["stats"].find_one({"name": f"username-{ad['username']}"})
-    response = client.post("/Impression/", json=ad)
+    el = db["stats"].find_one({"name": f"username-{TEST_AD['username']}"})
+    response = client.post("/Impression/", json=TEST_AD)
     assert response.status_code == 200
-    update_el = db["stats"].find_one({"name": f"username-{ad['username']}"})
+    update_el = db["stats"].find_one({"name": f"username-{TEST_AD['username']}"})
     assert el['impressions'] + 1 == update_el['impressions']
 
 
 def test_get_stats():
+    client.post("/GetAd/", json=TEST_AD)
+    client.post("/Impression/", json=TEST_AD)
+
     response = client.get("/GetStats/", params={"filter_type": 'username'})
 
     json_response = response.json()
